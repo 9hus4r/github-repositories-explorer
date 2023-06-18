d8b76bec04473f2c4303b83ad716b5740c71f6e8
agung.perdana <agung.perdana@mdi.id>
2023-06-19 00:22:09 +0700

    add unit test

--- src/__mocks__/axios.ts
+++
@@ -1,7 +0,0 @@
-import { mockRepositories } from './repositories'
-
-const mockRepositoriesResponse = (username: string) => mockRepositories.filter((repo) => repo.owner.login == username)
-
-export default {
-  getRepositories: (username: string) => jest.fn().mockResolvedValue(mockRepositoriesResponse(username)),
-}
